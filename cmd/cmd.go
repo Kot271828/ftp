8c154510f66b8c239b38556d21e668f775a467d4
Kot271828 <nbtgp528g@gmail.com>
2023-03-26 18:15:08 +0900

    Add cmd.Type enum.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,6 +2,36 @@ package cmd
 
 import "strings"
 
+type Type int
+
+const (
+	USER Type = iota
+	QUIT
+	PORT
+	STRU
+	RETR
+	NOOP
+)
+
+func (t Type) String() string {
+	var str string
+	switch t {
+	case USER:
+		str = "USER"
+	case QUIT:
+		str = "QUIT"
+	case PORT:
+		str = "PORT"
+	case RETR:
+		str = "RETR"
+	case NOOP:
+		str = "NOOP"
+	default:
+		panic("There is a const having no String method.")
+	}
+	return str
+}
+
 func Parse(s string) (string, []string) {
 	args := strings.Split(strings.Trim(s, " "), " ")
 	cmd := strings.ToUpper(args[0])
